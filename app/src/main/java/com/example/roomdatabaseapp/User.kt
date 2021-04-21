84903d550a2ebb7d7766f70c492b056a93beb807
Savo Lekovic <savolekovic15@gmail.com>
2021-04-21 11:52:01 +0200

    commit everything

--- app/src/main/java/com/example/roomdatabaseapp/User.kt
+++ app/src/main/java/com/example/roomdatabaseapp/User.kt
@@ -1,5 +1,6 @@
 package com.example.roomdatabaseapp
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -9,8 +10,10 @@ data class User(
     @PrimaryKey(autoGenerate = true)
     var userId: Long = 0L,
 
+    @ColumnInfo(name = "first_name")
     val firstName: String,
 
+    @ColumnInfo(name = "last_name")
     val lastName: String,
 
 )
